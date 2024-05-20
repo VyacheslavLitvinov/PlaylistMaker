751c05b46ba67d9e9e7e7b86da907a77691bd3ea
Vyacheslav Litvinov <rovyacheslav@gmail.com>
2024-05-20 23:54:38 +0200

    добавил сервис реестра треков из itunes

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,69 +1,90 @@
 package com.example.playlistmaker
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.ImageView
-import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
 
     var resultSearch : String = RESULT_DEF
+
+    private val iTunesBaseUrl = "https://itunes.apple.com"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(iTunesBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val iTunesService = retrofit.create(ItunesSearchAPI::class.java)
+
+    private val songs = ArrayList<Song>()
+
+    private lateinit var placeholderImageNotResult: ImageView
+    private lateinit var placeholderImageNetwork: ImageView
+    private lateinit var placeholderMessage: TextView
+    private lateinit var editTextSearch: EditText
+    private lateinit var songList: RecyclerView
+    private lateinit var updateButton: Button
+
+    private val adapter = SearchAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
 
         val backButton = findViewById<FrameLayout>(R.id.back_button_search)
-        val editTextSearch = findViewById<EditText>(R.id.inputEditTextSearch)
         val clearButton = findViewById<ImageView>(R.id.clearIconSearch)
+        editTextSearch = findViewById(R.id.inputEditTextSearch)
+        placeholderMessage = findViewById(R.id.placeholderMessage)
+        songList = findViewById(R.id.tracks)
+        placeholderImageNotResult = findViewById(R.id.placeholderImageNotResult)
+        placeholderImageNetwork = findViewById(R.id.placeholderImageNetwork)
+        updateButton = findViewById(R.id.updateButton)
 
-        val listTrack = SearchAdapter(
-            arrayListOf<Track>(
-                Track("Smells Like Teen Spirit",
-                    "Nirvana",
-                    "5:01",
-                    "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b" +
-                            "51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-                Track("Billie Jean",
-                    "Michael Jackson",
-                    "4:35",
-                    "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-" +
-                            "71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-                Track("Stayin' Alive",
-                    "Bee Gees",
-                    "4:10",
-                    "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-" +
-                            "8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-                Track("Whole Lotta Love",
-                    "Led Zeppelin",
-                    "5:33",
-                    "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-" +
-                            "2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-                Track("Sweet Child O'Mine",
-                    "Guns N' Roses",
-                    "5:03",
-                    "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-" +
-                            "03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
-            )
-        )
+        adapter.songs = songs
+        songList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        songList.adapter = adapter
 
         clearButton.setOnClickListener {
             editTextSearch.setText("")
             hideKeyboard(editTextSearch)
             editTextSearch.clearFocus()
+            songs.clear()
+            adapter.notifyDataSetChanged()
+            placeholderMessage.visibility = View.GONE
+            placeholderImageNotResult.visibility = View.GONE
+            placeholderImageNetwork.visibility = View.GONE
+            updateButton.visibility = View.GONE
+        }
+
+        updateButton.setOnClickListener {
+            search()
+        }
+
+        editTextSearch.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                search()
+            }
+            false
         }
 
         val textWatcher = object : TextWatcher{
@@ -88,8 +109,31 @@ class SearchActivity : AppCompatActivity() {
             finish()
         }
 
-        val tracks =findViewById<RecyclerView>(R.id.tracks)
-        tracks.adapter = listTrack
+    }
+
+    private fun search() {
+        iTunesService.search(editTextSearch.text.toString()).enqueue(object : Callback<SongsResponse> {
+            override fun onResponse(call: Call<SongsResponse>, response: Response<SongsResponse>) {
+                if (response.code() == 200) {
+                    songs.clear()
+                    if (response.body()?.results?.isNotEmpty() == true) {
+                        songs.addAll(response.body()?.results!!)
+                        adapter.notifyDataSetChanged()
+                    }
+                    if (songs.isEmpty()) {
+                        showMessage(getString(R.string.nothing_found))
+                    } else {
+                        showMessage("")
+                    }
+                } else {
+                    showMessage(getString(R.string.something_went_wrong))
+                }
+            }
+
+            override fun onFailure(call: Call<SongsResponse>, t: Throwable) {
+                showMessage(getString(R.string.network_problems))
+            }
+        })
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
@@ -103,6 +147,28 @@ class SearchActivity : AppCompatActivity() {
     private fun hideKeyboard(view: View) {
         val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun showMessage(text: String) {
+        if (text.isNotEmpty()) {
+            placeholderMessage.visibility = View.VISIBLE
+            songs.clear()
+            adapter.notifyDataSetChanged()
+            placeholderMessage.text = text
+            if (placeholderMessage.text == getString(R.string.network_problems)) {
+                placeholderImageNetwork.visibility = View.VISIBLE
+                placeholderImageNotResult.visibility = View.GONE
+                updateButton.visibility = View.VISIBLE
+            } else if (placeholderMessage.text == getString(R.string.nothing_found)) {
+                placeholderImageNotResult.visibility = View.VISIBLE
+                placeholderImageNetwork.visibility = View.GONE
+            }
+        } else {
+            placeholderMessage.visibility = View.GONE
+            placeholderImageNotResult.visibility = View.GONE
+            placeholderImageNetwork.visibility = View.GONE
+            updateButton.visibility = View.GONE
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
